d3535cd350197c7dd8d2a0ed2eb77fdd565a52ed
nick-yushenko <59775649+nick-yushenko@users.noreply.github.com>
2021-07-01 02:10:10 +0700

    Add files via upload

--- mail.php
+++ mail.php
@@ -15,7 +15,7 @@ $mail->SMTPAuth = true;                               // Enable SMTP authenticat
 $mail->Username = 'adm.vtb@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
 $mail->Password = 'boom1234'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+$mail->Port = 587; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
 $mail->setFrom('adm.vtb@mail.ru'); // от кого будет уходить письмо?
 $mail->addAddress('nickita_yushenko@mail.ru');     // Кому будет уходить письмо 
