548edee020031e23e13f2b7448032d8b7bc64360
nick-yushenko <59775649+nick-yushenko@users.noreply.github.com>
2021-07-01 18:04:35 +0700

    Add files via upload

--- mail.php
+++ mail.php
@@ -20,7 +20,8 @@ $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, 
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
 $mail->setFrom('adm.vtb@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('nickita_yushenko@mail.ru');     // Кому будет уходить письмо 
+$mail->addAddress('henrykopanev@gmail.com');     // Кому будет уходить письмо 
+// $mail->addAddress('nickita_yushenko@mail.ru');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
@@ -29,17 +30,17 @@ $mail->addAddress('nickita_yushenko@mail.ru');     // Кому будет ухо
 //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 $mail->isHTML(true);                                  // Set email format to HTML
 
-$mail->Subject = 'Заявка с тестового сайта';
-$mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone. '<br>Почта этого пользователя: ' .$email;
+$mail->Subject = 'Заявка с сайта: Заявка на открытие счета';
+$mail->Body    = 'Контактный телефон: ' .$phone . '<br> Ваши имя и фамилия: ' . $name. '<br> Подтверждаю согласие с условиями обработки персональных данных: yes <br> Источник: Интернет <br> Продукт: Горячий Расчетно-кассовое обслуживание <br> utm_source: marketplace_boomstarter <br> Номер заявки: 1234567890 '; 
 $mail->AltBody = '';
 
 if(!$mail->send()) {
     echo 'Error';
 } else {
-    header('location: thank-you.html');
+    header('location: index.html');
 }
 ?>
 
 
-<!-- старый код  -->
+
 
